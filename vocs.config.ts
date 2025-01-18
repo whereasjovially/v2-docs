99ed10ee6de4b41b40d9e11f1c15fcf831413514
irfanshaik <irfanshaik11@gmail.com>
2025-01-18 04:38:41 -0500

    more

--- vocs.config.ts
+++ vocs.config.ts
@@ -4,16 +4,34 @@ export default defineConfig({
   title: 'Interstate Docs',
   sidebar: [
     {
+      text: 'Intro',
+      link: '/intro',
+    },
+    {
+      text: 'For Validators',
+      items: [
+        {
+          text: 'Quickstart',
+          link: '/validator/quickstart',
+        },
+        {
+          text: 'Advanced Configuration',
+          link: '/validator/advanced',
+        },
+        {
+          text: 'Verify ',
+          link: '/validator/troubleshooting',
+        }
+      ]
+    },
+
+    {
       text: 'Validator Quickstart',
       link: '/validator-quickstart',
     },
     {
       text: 'API Specification',
       link: '/api-specification',
-    },
-    {
-      text: 'Intro',
-      link: '/intro',
     },
   ],
 })
